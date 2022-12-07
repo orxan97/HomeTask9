9d5a806e36a70ee3981175f8b3cbdc0028c2632a
orxan97 <orkhanna@code.edu.az>
2022-12-07 18:05:44 +0400

    Add project files.

---
+++ HomeTask9/Extension.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTask9
+{
+    public static class Extension
+    {
+         public static bool CustomContains(this string str,string searchtext)
+        {
+            List<char> chars = new List<char>();
+            foreach (var item in searchtext)
+            {
+                foreach (var item1 in str)
+                {
+                    if (item.ToString().ToLower() == item1.ToString().ToLower())
+                    {
+                        chars.Add('t');
+                    }
+                }
+
+            }
+            if (chars.Count==str.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        
+    }
+}
---
+++ HomeTask9/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HomeTask9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Bu bir custom contains methodudur";
+
+            string newstr = "bu bir";
+
+
+            Console.WriteLine(str.CustomContains(newstr));
+        }
+
+    }
+}
